1432c0ad1a654c4e54ceb712883137004cc2f546
Caleb Dawson <calebdawsondev@gmail.com>
2024-03-24 17:52:46 -0400

    Create Pages

--- code.ts
+++ code.ts
@@ -7,12 +7,37 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__uiFiles__.main, {width: 1000, height: 750,});
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+figma.ui.onmessage =  (msg: {type: string}) => {
+  if (msg.type === 'name') {
+    figma.showUI(__uiFiles__.name, {width: 1000, height: 750,});
+  }
+  if (msg.type === 'email') {
+    figma.showUI(__uiFiles__.email, {width: 1000, height: 750,});
+  }
+  if (msg.type === 'phone') {
+    figma.showUI(__uiFiles__.phone, {width: 1000, height: 750,});
+  }
+  if (msg.type === 'physical') {
+    figma.showUI(__uiFiles__.physical, {width: 1000, height: 750,});
+  }
+  if (msg.type === 'lorem') {
+    figma.showUI(__uiFiles__.lorem, {width: 1000, height: 750,});
+  }
+  if (msg.type === 'main') {
+    figma.showUI(__uiFiles__.main, {width: 1000, height: 750,});
+  }
+  if (msg.type === 'close') {
+    figma.closePlugin();
+  }
+}
+
+
+/* figma.ui.onmessage =  (msg: {type: string, count: number}) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
   if (msg.type === 'create-rectangles') {
@@ -31,4 +56,4 @@ figma.ui.onmessage =  (msg: {type: string, count: number}) => {
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
-};
+}; */
